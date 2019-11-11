abd88f2fbf9482d2973db6f212f263c82543767d
MatheusEdnei <ednei880@gmail.com>
2019-11-11 20:51:47 -0300

    Melhorando o layout de index.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+	<!-- Footer -->
+<div class="footer_overlay"></div>
+<footer class="footer">
+    <div class="footer_background" style="background-image:url(images/footer.jpg)"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center">
+                    <div class="footer_logo"><a href="#">AllInOne</a></div>
+                    <div class="footer_social ml-lg-auto">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
